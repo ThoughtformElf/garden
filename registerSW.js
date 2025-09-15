be92d0aecce7aecb8e16bb50127da12a8fe9bf42
thoughtforms <thoughtforms@users.noreply.github.com>
2025-09-15 04:43:10 +0000

    deploy: da783976dd8431e8e0022bc732a24678b4b00b8b

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/garden/sw.js', { scope: '/garden/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
