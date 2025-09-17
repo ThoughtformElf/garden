c62855a4ead35bc37804b02ac271c8d5a6945d16
Thoughtform <thoughtformgarden@proton.me>
2025-09-17 16:38:13 -0700

    horitzontal rule styles

--- src/editor/plugins/index.js
+++ src/editor/plugins/index.js
@@ -1,9 +1,11 @@
+// src/editor/plugins/index.js
 import { hashtagPlugin } from './hashtags.js';
 import { wikilinkPlugin } from './wikilinks.js';
 import { checkboxPlugin } from './checkboxes.js';
 import { timestampPlugin } from './timestamps.js';
 import { linkPlugin } from './links.js';
 import { blockquotePlugin } from './blockquotes.js';
+import { rulerPlugin } from './ruler.js';
 
 export const allHighlightPlugins = [
   hashtagPlugin,
@@ -12,4 +14,5 @@ export const allHighlightPlugins = [
   timestampPlugin,
   linkPlugin,
   blockquotePlugin,
+  rulerPlugin,
 ];
---
+++ src/editor/plugins/ruler.js
@@ -0,0 +1,44 @@
+// src/editor/plugins/ruler.js
+import { ViewPlugin, Decoration } from '@codemirror/view';
+import { RangeSetBuilder } from '@codemirror/state';
+
+// Use Decoration.line() to apply a style to the entire line
+const rulerDecoration = Decoration.line({ class: 'cm-hr' });
+
+export const rulerPlugin = ViewPlugin.fromClass(
+  class {
+    decorations;
+
+    constructor(view) {
+      this.decorations = this.findRulers(view);
+    }
+
+    update(update) {
+      if (update.docChanged || update.viewportChanged) {
+        this.decorations = this.findRulers(update.view);
+      }
+    }
+
+    findRulers(view) {
+      const builder = new RangeSetBuilder();
+      // Regex to find lines consisting of 3 or more hyphens, equals signs, or asterisks
+      const rulerRegex = /^\s*([-=*_]){3,}\s*$/;
+
+      for (const { from, to } of view.visibleRanges) {
+        let pos = from;
+        while (pos <= to) {
+          const line = view.state.doc.lineAt(pos);
+          if (rulerRegex.test(line.text)) {
+            // Add the line decoration from the start of the line
+            builder.add(line.from, line.from, rulerDecoration);
+          }
+          pos = line.to + 1; // Move to the next line
+        }
+      }
+      return builder.finish();
+    }
+  },
+  {
+    decorations: v => v.decorations
+  }
+);
